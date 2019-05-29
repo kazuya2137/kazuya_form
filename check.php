2d38448af06898dfeafda35f94b9477db7ed09ad
和也山本 <kazuya2137@gmail.com>
2019-05-29 14:50:53 +0800

    フォームを表示
    確認画面を追加

--- check.php
+++ check.php
@@ -1,1 +1,49 @@
 <?php
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+// スーパーグローバル関数
+// echo $nickname;
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+        # code...
+    }else{
+        $nickname_result = 'ようこそ、' . $nickname .'様';
+
+    }
+    
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+        # code...
+    }else{
+        $email_result = 'メールアドレス' . $email;
+
+    }
+    
+        # code...
+    if ($contact == '') {
+        $contact_result = 'お問い合わせ内容が入力されていません。';
+        # code...
+    }else{
+        $contact_result = 'お問い合わせ内容' . $contact;
+
+    }
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>入力内容確認</title>
+</head>
+<body>
+    <h1>入力内容確認</h1>
+    <p><?php echo $nickname_result ?></p>
+    <p><?php echo $email_result ?></p>
+    <p><?php echo $contact_result ?></p>
+
+
+</body>
+</html>
